0ab7fa8eb3f3e8509e821e19a32ef815f62620dd
OllegK <oleg.koleskin@gmail.com>
2019-11-30 20:14:15 +0200

    changed script to include the new extension

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
+npm run migrate export --verbose email_old_ext_2019_11_30.csv hcdbfckhdcpepllecbkaaojfgipnpbpb
 
-
-#npm run migrate export --verbose test.csv bmodgidfcfofhijhekfpmpkpdkemmfal
-
-npm run migrate export --verbose email_old_ext_2019_11_02.csv hcdbfckhdcpepllecbkaaojfgipnpbpb
+npm run migrate export --verbose email_new_ext_2019_11_30.csv clibiflfecckdjnjcgcgjdknmbgceail
 
 #npm run migrate export --verbose extensions2019_08_17.csv extensions
